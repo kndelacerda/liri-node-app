af27f76f6b803470516065adc8a932b363c422c7
Kristen De La Cerda <kndelacerda@gmail.com>
2017-05-15 17:02:13 -0500

    Revisions

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-    consumer_key: '<input here>',
-    consumer_secret: '<input here>',
-    access_token_key: '<input here>',
-    access_token_secret: '<input here>',
+    consumer_key: 'RM9L0Ia6BvkQ4ZXma6oGGHrsV',
+    consumer_secret: 'Brv2YP8R9OK4rzY2Lr4OjfWaGBA2pMX3Q9lnsJccPaizL3JBvk',
+    access_token_key: '863898071788343296-JGAHztgfHrf7lMIZw4NlgsxytDNCU77',
+    access_token_secret: 'UkIkAlImpl8gYR4Wc7oCb7X56h4HevkGd7aj3NjQj0RbW',
 };
