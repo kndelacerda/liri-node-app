f2326e1cff67d9386c796fa2365b01cf95f78c80
Kristen De La Cerda <kndelacerda@gmail.com>
2017-05-22 19:13:28 -0500

    Removed comma

--- keys.js
+++ keys.js
@@ -4,5 +4,7 @@ exports.twitterKeys = {
     consumer_key: 'RM9L0Ia6BvkQ4ZXma6oGGHrsV',
     consumer_secret: 'Brv2YP8R9OK4rzY2Lr4OjfWaGBA2pMX3Q9lnsJccPaizL3JBvk',
     access_token_key: '863898071788343296-JGAHztgfHrf7lMIZw4NlgsxytDNCU77',
-    access_token_secret: 'UkIkAlImpl8gYR4Wc7oCb7X56h4HevkGd7aj3NjQj0RbW',
+    access_token_secret: 'UkIkAlImpl8gYR4Wc7oCb7X56h4HevkGd7aj3NjQj0RbW'
 };
+
+// Replace the keys with a space once finalized on Github! 
